e1e67483125c31087bc8a8df88bea91deb8d41be
Maruf Islam <maruf35-2195@diu.edu.bd>
2023-06-05 06:31:55 +0600

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=> fake()->title(),
+            'price'=> fake()->randomElement([10000, 20000, 50000]),
+            'quantity'=> '20',
+            'category'=> 'phone',
+            'description'=> 'my description',
+        ];
+    }
+}
